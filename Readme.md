0b4cf2c8a64b8c1a58141070e34fd45e9f791bd8
starcoc3515 <89995594+starcoc3515@users.noreply.github.com>
2021-09-02 18:49:22 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+english
+This bot are create by starcoc3515
+chanel youtube starcoc 3515
+bot discord name:mdstarbot
+starcoc3515 discord:starcoc3#7854
+Français
+Ce bot a été créer par starcoc 3515
+chanel youtube starcoc 3515
+le nom du bot discord est mdsarbot
+le discord de starcoc3515 est:starcoc3#7854
---
+++ index.js
@@ -0,0 +1,85 @@
+const { ifError } = require("assert");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const Discord = require("discord.js");
+const Client = new Discord.Client({
+    intents:[
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+const data = new SlashCommandBuilder()
+    .setName("chaine")
+    .setDescription("renvoie la chaine")
+    .addUserOption(option => option.setName("utilisateur").setDescription("user que vous soiter mentionner").setRequired(false))
+
+const prefix = "!";
+
+Client.on("ready", async () =>{
+
+    Client.guilds.cache.get("880140450852401193").commands.create(data);
+//achanger en casde changement de server (880140450852401193)
+console.log(Client.guilds.cache.get("880140450852401193").commands.fetch());
+    await Client.guilds.cache.get("880140450852401193").commands.fetch();
+    Client.guilds.cache.get("880140450852401193").commands.fetch()
+
+
+
+    console.log("Lancer");
+});
+
+Client.on("interactionCreate", interaction =>{
+    if(interaction.isCommand()){
+        if(interaction.commandName === "chaine"){
+            let user = interaction.options.getUser("utilisateur");
+
+            if(user != undefined){
+                //<@id>
+                interaction.reply("starcoc 3515 <@" + user.id + ">")
+            }
+            else {
+               interaction.reply("staroc 3515");
+            }
+        }
+    }
+});
+
+Client.on("messageCreate",message => {
+    if (message.author.bot) return;
+
+    //!help
+
+    if (message.content === prefix + "help"){
+        const embed = new Discord.MessageEmbed()
+            .setColor("#f00020")
+         .setTitle("Liste des commandes")
+         .setAuthor("Auteur du bot starcoc3")
+         .setDescription("Vous trouverer ici la liste des commande du bot")
+         .addField("!help","Affiche la liste des commande")
+         .setTimestamp();
+         
+         message.channel.send({ embeds: [embed]});
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Client.login("ODgwMTM5MzU0MzA2MTI5OTIw.YSZ7pg.8Fv2m7dQKNVgb0xiEa2M_MVbOrk");
